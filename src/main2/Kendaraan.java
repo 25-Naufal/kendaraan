395246765d073ac5f44e6f6cd7eee9c268972838
25-Naufal <naufal_rafif_29rpl@student.smktelkom-mlg.sch.id>
2021-02-05 10:14:29 +0700

    hore berhasil

---
+++ src/main2/Kendaraan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main2;
+
+/**
+ *
+ * @author ASUS X450JN
+ */
+public class Kendaraan {
+   
+    
+
+    static void Kendaraan() {
+        String brand = "Mizuhara";
+        String jenis = "Sports";
+        String cc = "3700"; 
+        System.out.println("Brand bernama "+brand+" yang berjenis "+jenis+" dan juga bertenaga "+cc);
+    }
+
+
+}
